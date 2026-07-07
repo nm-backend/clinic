0706a17aa6dcac7d3643ed21183a1c1e90db796d
Nurullo <nurullomusajanov395@gmail.com>
2026-07-07 16:39:20 +0600

    делаю секшон 'Наши специалисты'

--- clinics/urls.py
+++ clinics/urls.py
@@ -1,18 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 
-from clinics import views
+router = DefaultRouter()
+router.register(r'clinics', views.ClinicViewSet)
+router.register(r'doctors', views.DoctorViewSet)
+router.register(r'services', views.ServiceViewSet)
+router.register(r'appointments', views.AppointmentViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
-
-    path('clinics/', views.ClinicListAPIView.as_view(), name='api-clinics'),
-    path('doctors/', views.DoctorListAPIView.as_view(), name='api-doctors'),
-    path('service-categories/', views.ServiceCategoryListAPIView.as_view(), name='api-service-categories'),
-    path('services/', views.ServiceListAPIView.as_view(), name='api-services'),
-    path('available-slots/', views.AvailableSlotsAPIView.as_view(), name='api-available-slots'),
-    path('auth/register/', views.RegisterAPIView.as_view(), name='auth-register'),
-    path('auth/me/', views.CurrentUserAPIView.as_view(), name='auth-me'),
-    path('patients/me/appointments/', views.PatientAppointmentsAPIView.as_view(), name='patient-appointments'),
-    path('appointments/', views.AppointmentListCreateAPIView.as_view(), name='api-appointments'),
-    path('appointments/<int:pk>/', views.AppointmentRetrieveUpdateDestroyAPIView.as_view(), name='api-appointment-detail'),
+    path('', include(router.urls)),
 ]
